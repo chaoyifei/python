3eb43ac2e8d54cf051478cd27bf7ff2a6426edd2
chaoyifei <451089603@qq.com>
2017-07-17 14:31:39 +0800

    两个测试打开一次浏览器，@classmethod

--- selenium/automation_framework_demo/automation_framework_demo/testsuit/baidu_search.py
+++ selenium/automation_framework_demo/automation_framework_demo/testsuit/baidu_search.py
@@ -11,23 +11,32 @@ from pageobjects.baidu_homepage import HomePage
 
 class BaiduSearch(unittest.TestCase):  
   
-  
+    '''
     def setUp(self):  
-        '''
-                        测试固件的setUp()的代码，主要是测试的前提准备工作 
         
-        ''' 
+        测试固件的setUp()的代码，主要是测试的前提准备工作 
+        
+        
         browse = BrowserEngine(self)  
         self.driver = browse.open_browser(self)  
-  
-  
+    '''
+    @classmethod
+    def setUpClass(cls):
+        browse=BrowserEngine(cls)
+        cls.driver=browse.open_browser(cls)
+    '''
     def tearDown(self):  
-        ''' 
+        
                             测试结束后的操作，这里基本上都是关闭浏览器 
          
-        ''' 
+        
         self.driver.quit()  
-  
+    '''
+   
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        
     def test_baidu_search(self):  
         '''
                           这里一定要test开头，把测试逻辑代码封装到一个test开头的方法里。 
@@ -52,7 +61,18 @@ class BaiduSearch(unittest.TestCase):
             print ('Test Pass.')  
         except Exception as e:  
             print ('Test Fail.', format(e))  
-  
+    #添加第二个人测试
+    
+    def test_search2(self):
+        homepage=HomePage(self.driver)
+        homepage.type_search('python')
+        homepage.send_submit_btn()
+        time.sleep(2)
+        homepage.get_windows_img()
+        
+        
+        
+        
 if __name__ == '__main__':  
     unittest.main()  
    
