167cf90f62cd91e76a775e28759bd944d97668ed
chaoyifei <451089603@qq.com>
2017-10-17 15:43:52 +0800

    push

--- Linux/linux-cmd.py
+++ Linux/linux-cmd.py
@@ -8,8 +8,8 @@
 '''
 import paramiko
 import time
-#定义通道
-def linux_cmd(host_ip,username,password,timeouts,cmd,info,coun,end):
+#带交互的通道
+def linux_interactive(host_ip,username,passwd,timeouts,cmd,info,coun,end):
     #定义接收缓存大小
     recv_buffer = 1024
     # 设置ssh连接的远程主机地址和端口
@@ -33,8 +33,37 @@ def linux_cmd(host_ip,username,password,timeouts,cmd,info,coun,end):
         time.sleep(2)
     else:
         pass
+
         chan.send(coun + '\n')
         str = chan.recv(recv_buffer)
         while not str.endswith(end):
             print  chan.recv(recv_buffer)
+    chan.close()
     t.close()
+#执行命令的通道：
+def linux_cmd(host_ip,username,passwd,timeouts,cmd,info,coun,end):
+    #定义接收缓存大小
+    recv_buffer = 1024
+    # 设置ssh连接的远程主机地址和端口
+    t = paramiko.Transport(host_ip, 22)
+    # 设置登录名和密码
+    t.connect(username=username, password=passwd)
+    # 连接成功后打开一个channel
+    chan = t.open_session()
+    # 设置会话超时时间
+    chan.settimeout(timeouts)
+    # 打开远程的terminal
+    chan.get_pty()
+    # 激活terminal
+    chan.invoke_shell()
+    chan.send(cmd + '\n')
+    time.sleep(2)
+    strs = chan.recv(recv_buffer)
+    print strs
+    str = chan.recv(recv_buffer)
+    while not str.endswith(end):
+        print  chan.recv(recv_buffer)
+    chan.close()
+    t.close()
+
+
